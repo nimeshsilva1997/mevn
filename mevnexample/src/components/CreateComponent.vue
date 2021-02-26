b75240a4d138bce567d4d24fd37dd2729dd26ad4
Nimesh Ryan Silva <nimeshsilva@gmail.com>
2021-02-26 12:21:11 -0500

    Update CreateComponent.vue

--- mevnexample/src/components/CreateComponent.vue
+++ mevnexample/src/components/CreateComponent.vue
@@ -42,11 +42,12 @@ export default {
     };
   },
   methods: {
-
-addPost(){
-    let uri = 'http://localhost:4000/posts/add';
-    this.axios.post(uri, this.post).then(() => {
-       this.$router.push({name: 'posts'});
-    });
-}
+    addPost() {
+      let uri = "http://localhost:4000/posts/add";
+      this.axios.post(uri, this.post).then(() => {
+        this.$router.push({ name: "posts" });
+      });
+    },
+  },
+};
 </script>
